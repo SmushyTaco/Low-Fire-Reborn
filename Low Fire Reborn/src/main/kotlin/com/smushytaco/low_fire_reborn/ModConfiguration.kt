bef6394e1485e6179062b82ada2751ba0fcf0b00
Nikan Radan <personal@nikanradan.com>
2024-11-25 18:33:33 -0800

    New Config

---
+++ Low Fire Reborn/src/main/kotlin/com/smushytaco/low_fire_reborn/ModConfiguration.kt
@@ -0,0 +1,19 @@
+package com.smushytaco.low_fire_reborn
+import io.wispforest.owo.config.annotation.*
+@Modmenu(modId = LowFire.MOD_ID)
+@Config(name = LowFire.MOD_ID, wrapperName = "ModConfig")
+@Suppress("UNUSED")
+class ModConfiguration {
+    @JvmField
+    var enableLowFire = true
+    @JvmField
+    var shouldRenderFire = true
+    @JvmField
+    var fireOffset = -0.3
+    @JvmField
+    var fireOffsetCycleUpperKeyLimit = 0.0
+    @JvmField
+    var fireOffsetCycleLowerKeyLimit = -0.5
+    @JvmField
+    var fireOffsetChange = 0.1
+}
--- Low Fire Reborn/src/main/kotlin/com/smushytaco/low_fire_reborn/configuration_support/ModMenuIntegration.kt
+++
@@ -1,8 +0,0 @@
-package com.smushytaco.low_fire_reborn.configuration_support
-import com.terraformersmc.modmenu.api.ConfigScreenFactory
-import com.terraformersmc.modmenu.api.ModMenuApi
-import me.shedaniel.autoconfig.AutoConfig
-class ModMenuIntegration: ModMenuApi {
-    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> =
-        ConfigScreenFactory { parent -> AutoConfig.getConfigScreen(ModConfiguration::class.java, parent).get() }
-}
