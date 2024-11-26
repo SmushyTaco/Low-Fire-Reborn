a99243f07d11fb7a5efc2ac040a5f4d320a8fe0f
Nikan Radan <github.com@alias.nikanradan.com>
2024-11-25 18:33:33 -0800

    New Config

--- Low Fire Reborn/src/main/kotlin/com/smushytaco/low_fire_reborn/LowFire.kt
+++ Low Fire Reborn/src/main/kotlin/com/smushytaco/low_fire_reborn/LowFire.kt
@@ -1,9 +1,4 @@
 package com.smushytaco.low_fire_reborn
-import com.smushytaco.low_fire_reborn.configuration_support.ModConfiguration
-import me.shedaniel.autoconfig.AutoConfig
-import me.shedaniel.autoconfig.ConfigHolder
-import me.shedaniel.autoconfig.annotation.Config
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
@@ -14,9 +9,7 @@ import org.lwjgl.glfw.GLFW
 import kotlin.math.abs
 object LowFire: ClientModInitializer {
     const val MOD_ID = "low_fire_reborn"
-    private lateinit var configHolder: ConfigHolder<ModConfiguration>
-    lateinit var config: ModConfiguration
-        private set
+    val config = ModConfig.createAndLoad()
     private val TOGGLE_KEYBINDING = KeyBinding("key.$MOD_ID.toggle", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "category.$MOD_ID.$MOD_ID")
     private val TOGGLE_RENDER_KEYBINDING = KeyBinding("key.$MOD_ID.toggle_render", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "category.$MOD_ID.$MOD_ID")
     private val CYCLE_FIRE_HEIGHT_KEYBINDING = KeyBinding("key.$MOD_ID.cycle_fire_height", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "category.$MOD_ID.$MOD_ID")
@@ -29,16 +22,11 @@ object LowFire: ClientModInitializer {
         KeyBindingHelper.registerKeyBinding(CYCLE_FIRE_HEIGHT_KEYBINDING)
         KeyBindingHelper.registerKeyBinding(RAISE_FIRE_KEYBINDING)
         KeyBindingHelper.registerKeyBinding(LOWER_FIRE_KEYBINDING)
-        AutoConfig.register(ModConfiguration::class.java) { definition: Config, configClass: Class<ModConfiguration> ->
-            GsonConfigSerializer(definition, configClass)
-        }
-        configHolder = AutoConfig.getConfigHolder(ModConfiguration::class.java)
-        config = configHolder.config
         ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick {
             val player = it.player ?: return@EndTick
             while(TOGGLE_KEYBINDING.wasPressed()) {
                 config.enableLowFire = !config.enableLowFire
-                configHolder.save()
+                config.save()
                 player.sendMessage(Text.of("Low Fire is now ${if (config.enableLowFire) "on" else "off"}!"), true)
             }
         })
@@ -46,7 +34,7 @@ object LowFire: ClientModInitializer {
             val player = it.player ?: return@EndTick
             while(TOGGLE_RENDER_KEYBINDING.wasPressed()) {
                 config.shouldRenderFire = !config.shouldRenderFire
-                configHolder.save()
+                config.save()
                 player.sendMessage(Text.of("Fire Rendering is now ${if (config.shouldRenderFire) "on" else "off"}!"), true)
             }
         })
@@ -75,7 +63,7 @@ object LowFire: ClientModInitializer {
                         config.fireOffset = String.format("%.2f", config.fireOffset - abs(config.fireOffsetChange)).toDouble()
                     }
                 }
-                configHolder.save()
+                config.save()
                 player.sendMessage(Text.of("The fire offset has been set to ${String.format("%.2f", config.fireOffset)}!"), true)
             }
         })
@@ -83,7 +71,7 @@ object LowFire: ClientModInitializer {
             val player = it.player ?: return@EndTick
             while(RAISE_FIRE_KEYBINDING.wasPressed()) {
                 config.fireOffset = String.format("%.2f", config.fireOffset + abs(config.fireOffsetChange)).toDouble()
-                configHolder.save()
+                config.save()
                 player.sendMessage(Text.of("The fire offset has been set to ${String.format("%.2f", config.fireOffset)}!"), true)
             }
         })
@@ -91,7 +79,7 @@ object LowFire: ClientModInitializer {
             val player = it.player ?: return@EndTick
             while(LOWER_FIRE_KEYBINDING.wasPressed()) {
                 config.fireOffset = String.format("%.2f", config.fireOffset - abs(config.fireOffsetChange)).toDouble()
-                configHolder.save()
+                config.save()
                 player.sendMessage(Text.of("The fire offset has been set to ${String.format("%.2f", config.fireOffset)}!"), true)
             }
         })
