20c10c8d77dd91711aa18697e0bce6cae68b9d6f
Nikan Radan <personal@nikanradan.com>
2024-12-13 01:16:33 -0800

    1.21.4

--- Low Fire Reborn/src/main/java/com/smushytaco/low_fire_reborn/mixins/InGameOverlayRendererMixin.java
+++ Low Fire Reborn/src/main/java/com/smushytaco/low_fire_reborn/mixins/InGameOverlayRendererMixin.java
@@ -1,7 +1,7 @@
 package com.smushytaco.low_fire_reborn.mixins;
 import com.smushytaco.low_fire_reborn.LowFire;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameOverlayRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,12 +10,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameOverlayRenderer.class)
 public class InGameOverlayRendererMixin {
 	@Inject(method = "renderFireOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;translate(FFF)V"))
-	private static void hookRenderFireOverlayOne(MinecraftClient client, MatrixStack matrices, CallbackInfo ci) {
+	private static void hookRenderFireOverlayOne(MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
 		if (!LowFire.INSTANCE.getConfig().getEnableLowFire()) return;
 		matrices.translate(0.0, LowFire.INSTANCE.getConfig().getFireOffset(), 0.0);
 	}
 	@Inject(method = "renderFireOverlay", at = @At("HEAD"), cancellable = true)
-	private static void hookRenderFireOverlayTwo(MinecraftClient client, MatrixStack matrices, CallbackInfo ci) {
+	private static void hookRenderFireOverlayTwo(MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
 		if (LowFire.INSTANCE.getConfig().getShouldRenderFire()) return;
 		ci.cancel();
 	}
